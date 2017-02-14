abe65e238c19017273befa37c97eafd78efd2318
aakuyr <issacnewton.sai@gmail.com>
2017-02-14 22:33:10 +0530

    Encryption algorithms

---
+++ schema.sql
@@ -0,0 +1,4 @@
+create table track{
+	user varchar2(20),
+ path varchar2(20)
+};
---
+++ 鍵.py
@@ -0,0 +1,78 @@
+import os
+import sqlite3
+from flask import Flask, redirect, request, session, url_for, abort, g, render_template, flash, send_from_directory
+
+#App config
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DATABASE = os.path.join(os.curdir, 'genius.db'),
+    SECRET_KEY = 'development',
+    USERNAME = 'sai',
+    PASSWORD = 'vasista'
+    )
+)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+#Connect to database
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+#Get the database for current application context
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+#Initialize database 
+@app.cli.command('init')
+def init_db():
+    db =get_db()
+    with app.open_resource('schema.sql', mode = 'r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+#Close the database
+def close_db():
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route that will process the file upload
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded file
+    file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = file.filename
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+        return redirect(url_for('uploaded_file',
+                                filename=filename))
+
+# This route is expecting a parameter containing the name
+# of a file. Then it will locate that file on the upload
+# directory and show it on the browser, so if the user uploads
+# an image, that image is going to be show after the upload
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
